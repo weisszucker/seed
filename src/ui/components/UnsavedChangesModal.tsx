f1920fc7754796eabc2a953cdf2f5359b8abd333
panzhihui <panzhihui3@huawei.com>
2026-02-21 21:52:28 +0800

    update prompt style

--- src/ui/components/UnsavedChangesModal.tsx
+++ src/ui/components/UnsavedChangesModal.tsx
@@ -7,40 +7,44 @@ type UnsavedChangesModalProps = {
 export function UnsavedChangesModal({ selectedOption, onChooseSave, onChooseDontSave }: UnsavedChangesModalProps) {
   return (
     <box position="absolute" left={0} top={0} width="100%" height="100%" justifyContent="center" alignItems="center" zIndex={100}>
-      <box width={56} border borderColor="#cd853f" padding={1} backgroundColor="#1f1f1f" flexDirection="column">
+      <box
+        width={56}
+        border
+        borderColor="#cd853f"
+        paddingLeft={1}
+        paddingRight={1}
+        paddingBottom={0}
+        paddingTop={0}
+        backgroundColor="#1f1f1f"
+        flexDirection="column"
+      >
         <box flexDirection="row" alignItems="center">
           <text fg="#ffd580">Unsaved changes</text>
           <box flexGrow={1} />
-          <box border borderColor="#666666" paddingLeft={1} paddingRight={1}>
-            <text fg="#aaaaaa">ESC</text>
+          <box paddingLeft={0} paddingRight={0} onMouseDown={onChooseDontSave}>
+            <text fg="#aaaaaa">esc</text>
           </box>
         </box>
-        <box flexDirection="row" gap={1}>
+        <box flexDirection="row" gap={1} justifyContent="flex-end" marginTop={2}>
           <box
-            width={24}
-            border
-            borderColor={selectedOption === "save" ? "#ffd580" : "#8fbc8f"}
+            width={12}
             backgroundColor={selectedOption === "save" ? "#2f2a1a" : "#1f1f1f"}
             onMouseDown={onChooseSave}
-            padding={1}
+            padding={0}
             alignItems="center"
             justifyContent="center"
           >
-            <text fg={selectedOption === "save" ? "#ffd580" : "#8fbc8f"}>{selectedOption === "save" ? "▶ Save" : "Save"}</text>
+            <text fg={selectedOption === "save" ? "#ffd580" : "#8fbc8f"}>Save</text>
           </box>
           <box
-            width={24}
-            border
-            borderColor={selectedOption === "dont_save" ? "#ffd580" : "#cd5c5c"}
+            width={12}
             backgroundColor={selectedOption === "dont_save" ? "#2f2a1a" : "#1f1f1f"}
             onMouseDown={onChooseDontSave}
-            padding={1}
+            padding={0}
             alignItems="center"
             justifyContent="center"
           >
-            <text fg={selectedOption === "dont_save" ? "#ffd580" : "#cd5c5c"}>
-              {selectedOption === "dont_save" ? "▶ Don't Save" : "Don't Save"}
-            </text>
+            <text fg={selectedOption === "dont_save" ? "#ffd580" : "#cd5c5c"}>Don't Save</text>
           </box>
         </box>
       </box>
